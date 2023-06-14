34720d8feb00b75e306c7679a256a4269fd6cb20
Matthew Rossi <mattw.rossi@gmail.com>
2023-06-14 18:07:07 +0200

    Segmentation fault :tada:

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 use std::{io::BufRead, io::BufReader};
 use std::mem;
@@ -24,10 +25,15 @@ const SRC_SIZE: usize = 32;
 
 #[derive(Parser)]
 struct Args {
-    // TODO: Add src and permission optional arguments
     // TODO: Print events as they come or aggregate them
-    // TODO: Export to CSV file
     // TODO: Add command output with optional argument
+    #[clap(
+        short('a'),
+        long("--aggregate"),
+        help("Print aggregate permission on exit. Otherwise, print events as \
+                they come with only the current permission")
+    )]
+    aggregate: bool,
     #[clap(required(true), help("Command run inside the sandbox."))]
     command: Vec<String>,
 }
@@ -73,7 +79,19 @@ pub extern "C" fn attach_tracer() {
     black_box(420);
 }
 
-fn event_handler(data: &[u8]) -> i32 {
+fn get_permission_string(encoded_permission: u8) -> String {
+    let mut permission = String::new();
+    permission.push(if encoded_permission & MAY_READ != 0 { 'r' } else { '-' });
+    permission.push(if encoded_permission & MAY_WRITE != 0 { 'w' } else { '-' });
+    permission.push(if encoded_permission & MAY_EXEC != 0 { 'x' } else { '-' });
+
+    permission
+}
+
+fn event_handler(
+    data: &[u8],
+    opt_path_permissions: Option<&mut HashMap<&str, u8>>
+) -> i32 {
     if data.len() != mem::size_of::<PathEvent>() {
         eprintln!(
             "Invalid size {} != {}",
@@ -87,16 +105,29 @@ fn event_handler(data: &[u8]) -> i32 {
     let event = unsafe { &*(data.as_ptr() as *const PathEvent) };
     let src = from_utf8(&event.src)
         .expect("Source should be UTF-8 encoded");
-    let path = from_utf8(&event.path)
+    let wrong_path = from_utf8(&event.path)
         .expect("Path should be UTF-8 encoded");
 
-    let mut permission = String::new();
-    permission.push(if event.permission & MAY_READ != 0 { 'r' } else { '-' });
-    permission.push(if event.permission & MAY_WRITE != 0 { 'w' } else { '-' });
-    permission.push(if event.permission & MAY_EXEC != 0 { 'x' } else { '-' });
-
     // Patch double printing issue
-    println!("{},{},{}", src, &path[..event.path_len as usize], permission);
+    let path = &wrong_path[..event.path_len as usize];
+
+    match opt_path_permissions {
+        Some(path_permissions) => {
+            match path_permissions.get(path) {
+                Some(permission) => {
+                    let new_permission = permission | event.permission;
+                    path_permissions.insert(path, new_permission);
+                },
+                None => {
+                    path_permissions.insert(path, event.permission);
+                }
+            }
+        },
+        None => {
+            let permission = get_permission_string(event.permission);
+            println!("{},{},{}", src, path, permission);
+        }
+    }
 
     return 0;
 }
@@ -117,46 +148,76 @@ fn main() -> Result<()> {
     // Load BPF programs
     let mut skel = OpensnoopSkelBuilder::default().open()?.load()?;
 
+    // Initialize data structure to aggregate path permissions
+    let mut path_permissions: HashMap<&str, u8> = HashMap::new();
+
     // Add ring buffer and associated callback
     let mut builder = libbpf_rs::RingBufferBuilder::new();
     builder
         .add(
             skel.maps().events(),
-            move | data | { event_handler(data) }
+            | data | {
+                event_handler(
+                    data,
+                    match args.aggregate {
+                        true => Some(&mut path_permissions),
+                        false => None,
+                    }
+                )
+            }
         )?;
-    let ring_buffer = builder.build()?;
 
-    // Attach BPF programs
-    skel.attach()?;
+    {
+        /* This code block ensure the ring_buffer is destroyed before we access
+        the path_permissions hash map in immutable way, thus preventing
+        concurrent memory modification leading to memory errors */
 
-    // Drop permitted capabilities
-    caps::clear(None, caps::CapSet::Permitted)?;
+        let ring_buffer = builder.build()?;
 
-    // Define tracing closure
-    let enable_tracing = move || {
-        attach_tracer();
-        Ok(())
-    };
+        // Attach BPF programs
+        skel.attach()?;
 
-    // Print header before running the command
-    println!("Hook,Path,Permission");
+        // Drop permitted capabilities
+        caps::clear(None, caps::CapSet::Permitted)?;
 
-    // Run command to trace
-    let program = &args.command[0];
-    let arguments = &args.command[1..];
-    unsafe {
-        // TODO: Redirect output and error to somewhere depending on user
-        // decision
-        let mut child = Command::new(program)
-            .args(arguments)
-            .pre_exec(enable_tracing)
-            .stdout(Stdio::null())
-            .stderr(Stdio::null())
-            .spawn()?;
+        // Define tracing closure
+        let enable_tracing = move || {
+            attach_tracer();
+            Ok(())
+        };
 
+        // Print header
+        match args.aggregate {
+            true => println!("Path,Permission"),
+            false => println!("Source,Path,Permission"),
+        }
+
+        // Run command to trace
+        let program = &args.command[0];
+        let arguments = &args.command[1..];
+        let mut child;
+        unsafe {
+            // TODO: Redirect output and error to somewhere depending on user
+            // decision
+            child = Command::new(program)
+                .args(arguments)
+                .pre_exec(enable_tracing)
+                .stdout(Stdio::null())
+                .stderr(Stdio::null())
+                .spawn()?;
+        }
+
+        // Listen for events while waiting for the child process to exit
         while child.try_wait()?.is_none() {
             // Use reasonable duration to avoid busy waiting
             ring_buffer.poll(core::time::Duration::from_millis(5))?;
+        }
+    }
+
+    if args.aggregate {
+        for (path, encoded_permission) in path_permissions {
+            let permission = get_permission_string(encoded_permission);
+            println!("{},{}", path, permission);
         }
     }
 
